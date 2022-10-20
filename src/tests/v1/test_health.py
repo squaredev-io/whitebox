681ddf2ae55068e866502b74717d49c167597984
sinnec <john.zaras@yahoo.com>
2022-10-20 13:18:38 +0300

    Tests: - Fixed 2 test - Commented 2 others - Moved pytest.ini file

--- src/tests/v1/test_health.py
+++ src/tests/v1/test_health.py
@@ -3,11 +3,6 @@ from src.tests.v1.conftest import test_order_map
 
 
 @pytest.mark.order(test_order_map["health"])
-def test_health(client, db, cron_client):
-    # response = cron_client.post(
-    #     "/v1/tasks/rdms_to_neo4j_intergration_pipeline/run",
-    #     headers={"X-API-KEY": "1234567890"},
-    # )
-
+def test_health(client):
     response = client.get("/v1/health")
-    assert response.json() == {"_": "Hello all possible worlds!", "status": 200}
+    assert response.json() == {"status": "OK"}
