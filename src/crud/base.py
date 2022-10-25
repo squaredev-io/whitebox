d749f54d96c47f46dc807b5d6bd42a139b564407
Megaklis Vasilakis <megaklis.vasilakis@gmail.com>
2022-10-25 14:07:32 +0300

    Fix update smelly code

--- src/crud/base.py
+++ src/crud/base.py
@@ -57,17 +57,14 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     ) -> ModelType:
         date_now = datetime.datetime.utcnow()
         obj_data = jsonable_encoder(db_obj)
-        #### Fix me
-        obj_in2 = jsonable_encoder(obj_in)
-        obj_in2 = {**obj_in2, "updated_at": date_now}
-        if isinstance(obj_in2, dict):
-            update_data = obj_in2
+        if isinstance(obj_in, dict):
+            update_data = obj_in
         else:
-            update_data = obj_in2.dict(exclude_unset=True)
+            update_data = obj_in.dict(exclude_unset=True)
         for field in obj_data:
             if field in update_data:
                 setattr(db_obj, field, update_data[field])
-        #######
+        setattr(db_obj, "updated_at", date_now)
         db.commit()
         db.refresh(db_obj)
         return db_obj
