083ab2c1982348a26cdaf5075b8a38d7f77023c8
Stavros Theocharis <stavros.theocharis@squaredev.io>
2022-11-01 16:39:22 +0200

    Adjusted requirements

--- src/analytics/metrics/pipelines.py
+++ src/analytics/metrics/pipelines.py
@@ -9,7 +9,6 @@ from evidently.report import Report
 from evidently.metric_preset import DataDriftPreset
 
 
-
 def create_feature_metrics_pipeline(
     dataset: pd.DataFrame,
 ) -> Dict[str, Union[int, float]]:
@@ -182,22 +181,29 @@ def create_multiple_classification_evaluation_metrics_pipeline(
         accuracy, precision_statistics, recall_statistics, f1_statistics, conf_matrix
     )
 
-def create_data_drift_pipeline(reference_dataset: pd.DataFrame,current_dataset: pd.DataFrame)-> Dict[str,Any]:
+
+def create_data_drift_pipeline(
+    reference_dataset: pd.DataFrame, current_dataset: pd.DataFrame
+) -> Dict[str, Any]:
     """
-    You will need two datasets. The reference dataset serves as a benchmark. We analyze the change by comparing the current 
-    production data to the reference data.
-    The dataset should include the features you want to evaluate for drift. The schema of both datasets should be identical.
-    - In the case of pandas DataFrame,all column names should be string
+    Two datasets are needed
+    The reference dataset serves as a benchmark.
+    An analysis happens comparing the current production data to the reference data.
+
+    The dataset should include the needed features to evaluate for drift.
+    The schema of both datasets should be identical.
+    - In the case of pandas DataFrame, all column names should be string
     - All feature columns analyzed for drift should have the numerical type (np.number)
     - Categorical data can be encoded as numerical labels and specified in the column_mapping.
     - DateTime column is the only exception. If available, it can be used as the x-axis in the plots.
-    You can potentially choose any two datasets for comparison. But keep in mind that only the reference dataset 
+
+    Potentially, any two datasets can be used for comparison. Only the reference dataset
     will be used as a basis for comparison.
 
     How it works
 
     To estimate the data drift Evidently compares the distributions of each feature in the two datasets.
-    Evidently applies statistical tests to detect if the distribution has changed significantly. There is a default 
+    Evidently applies statistical tests to detect if the distribution has changed significantly. There is a default
     logic to choosing the appropriate statistical test based on:
     - feature type: categorical or numerical
     - the number of observations in the reference dataset
@@ -219,10 +225,12 @@ def create_data_drift_pipeline(reference_dataset: pd.DataFrame,current_dataset: 
     """
     drift_report = Report(metrics=[DataDriftPreset()])
     drift_report.run(reference_data=reference_dataset, current_data=current_dataset)
-    initial_report=drift_report.json()
-    initial_report=json.loads(initial_report)
-    data_drift_report={}
-    data_drift_report["timestamp"]=initial_report["timestamp"]
-    data_drift_report["drift_summary"]=initial_report["metrics"]["DataDriftTable"]
-    
+
+    initial_report = drift_report.json()
+    initial_report = json.loads(initial_report)
+
+    data_drift_report = {}
+    data_drift_report["timestamp"] = initial_report["timestamp"]
+    data_drift_report["drift_summary"] = initial_report["metrics"]["DataDriftTable"]
+
     return data_drift_report
