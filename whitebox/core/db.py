f899410fa41eaba89e91b8b0f28d93c3dc1b5721
gcharis <charis.giaralis@gmail.com>
2023-01-24 16:42:52 +0200

    Improve connect function

--- whitebox/core/db.py
+++ whitebox/core/db.py
@@ -34,10 +34,13 @@ async def connect():
 
     admin_exists = crud.users.get_first_by_filter(db=db, username="admin")
     if not admin_exists:
-        api_key = token_hex(32)
-        if settings.SECRET_KEY:
-            secret_key = settings.SECRET_KEY.encode()
-            api_key = encrypt_api_key(api_key, secret_key)
+        plain_api_key = token_hex(32)
+        secret_key = settings.SECRET_KEY
+        api_key = (
+            encrypt_api_key(plain_api_key, secret_key.encode())
+            if secret_key
+            else plain_api_key
+        )
 
         obj_in = UserCreateDto(username="admin", api_key=api_key)
         crud.users.create(db=db, obj_in=obj_in)
