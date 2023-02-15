3f9e3c86668fafb87a4d0e5d7c6586ead3b020e6
Stavros Theocharis <stavros.theocharis@squaredev.io>
2023-02-16 00:29:36 +0200

    Added style to inferences dataframe

--- whitebox/streamlit/tabs/inferences.py
+++ whitebox/streamlit/tabs/inferences.py
@@ -3,11 +3,24 @@ from utils.transformation import convert_inference_dict_to_df
 from utils.export import structure
 
 
+def highlight_rows(row, pred_column):
+    actual = row.loc["actual"]
+    pred = row.loc[pred_column]
+    if actual != pred:
+        color = "#8FC4C7"
+    else:
+        color = ""
+
+    return ["background-color: {}".format(color) for r in row]
+
+
 def create_inferences_tab(inf, pred_column):
     """ """
-    with st.spinner("Loading model drift..."):
+    with st.spinner("Loading inferences..."):
         structure()
         st.title("Inferences")
 
         inf_df = convert_inference_dict_to_df(inf, pred_column)
+        # Style and mark the columns when actual is not equal to prediction
+        inf_df = inf_df.style.apply(lambda x: highlight_rows(x, pred_column), axis=1)
         st.dataframe(inf_df, width=1200, height=390)
