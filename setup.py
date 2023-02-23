dde84508b9918b2f7fd87a311d48b89053fd664b
Megaklis Vasilakis <megaklis.vasilakis@gmail.com>
2023-02-23 14:14:57 +0200

    ☝️ Bump version to 0.0.20

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 
-VERSION = "0.0.19"
+VERSION = "0.0.20"
 
 DESCRIPTION = "Whitebox is an open source E2E ML monitoring platform with edge capabilities that plays nicely with kubernetes"
 LONG_DESCRIPTION = (Path(__file__).parent / "README.md").read_text()
